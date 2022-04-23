b848342191b4bbe421a646d8e8c82873cfe48692
KentaroDobashi <popkentaro3232@gmail.com>
2022-04-23 16:32:58 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -3,6 +3,20 @@
   str.replaseAll("0", "・");
 };*/
 
-const toMorseCode = function (x) {
+/*const toMorseCode = function (x) {
   return x.replaceAll("0", "・");
+};*/
+
+//toMorseCode(010110); // "・1・11・"と表示される
+
+const slide = document.getElementById("slide");
+const right = document.getElementById("right");
+const left = document.getElementById("left");
+
+const campImages = ["url(./image/Camp-1.png)", "./image/camp-2.png"];
+
+let count = 0;
+
+const imgChange = function () {
+  slide.style.backgroundImage = campImages[count];
 };
