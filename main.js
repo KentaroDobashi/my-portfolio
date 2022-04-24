d1ba01ab3a00d28d8edf997896ba981542d64a1e
KentaroDobashi <popkentaro3232@gmail.com>
2022-04-24 18:10:38 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
-/*const toMorseCode = function () {
-  let str = "0101101";
-  str.replaseAll("0", "・");
-};*/
-
-/*const toMorseCode = function (x) {
-  return x.replaceAll("0", "・");
-};*/
-
-//toMorseCode(010110); // "・1・11・"と表示される
-
 const slide = document.getElementById("slide");
 const right = document.getElementById("right");
 const left = document.getElementById("left");
 
-const campImages = ["url(./image/Camp-1.png)", "./image/camp-2.png"];
+//使う画像
+const campImages = ["url(./image/Camp-1.png)", "url(./image/camp-2.png)"];
 
+//はいれつのがじお
 let count = 0;
 
+//画像を変更する
 const imgChange = function () {
   slide.style.backgroundImage = campImages[count];
+};
+// ＞をクリックするとカウントを変更
+right.onclick = function () {
+  // 配列の最後まで行ったら最初に戻す
+  if (count === campImages.length) {
+    count = 0;
+  } else {
+    count++;
+  }
+  imgChange();
+};
+// ＜をクリックするとカウントを変更
+left.onclick = function () {
+  //配列の最初まで行ったら最後に戻す
+  if (count === 0) {
+    count = campImages.length;
+  } else {
+    count--;
+  }
+  imgChange();
 };
